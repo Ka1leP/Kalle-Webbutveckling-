d85f28b229c69be9a3187a4b53b3c93c6b9e9738
Ka1leP <116098809+Ka1leP@users.noreply.github.com>
2022-12-13 20:45:55 +0100

    Add files via upload

---
+++ javascriptuppgift/js/testbed.js
@@ -0,0 +1,8 @@
+function changeText(){
+	var rubrik = document.getElemntById('rubrik');
+	rubrik.innerHTML= 'Du klickade';
+}
+function changeBg(){
+	var hej = document.getElemntById('hej')
+	hej.classList.add('bakgrund');
+}
